2e220df221951231a905ce42828243023e9f9cdf
mycurioussoul <anupama.jaria22@gmail.com>
2024-07-23 18:36:42 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/containerhealth', methods=['GET'])
+def health_check():
+    return jsonify({"status": "OK"})
+
+@app.route('/containername')
+def form():
+    return render_template('container_name_form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f"Container name is, {name}!"
+
+@app.route('/containergreet', methods=['POST'])
+def greet():
+    data = request.get_json()
+    print (data)
+    name = data.get('name', 'World')
+    return jsonify({"message": f"Container name is, {name}!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
